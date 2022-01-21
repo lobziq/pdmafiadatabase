ba0b0fce9bd03bf54b5495f7bcc54c861fb608a4
Artem Lobziq <lobziq@gmail.com>
2022-01-22 02:39:31 +0300

    project restructure to meet github pages site constraints

---
+++ src/Navbar.svelte
@@ -0,0 +1,13 @@
+<main>
+    <nav class="navbar" aria-label="main navigation">
+        <div class="navbar-menu align-center">
+            <a href="todo" class="navbar-item">Игроки</a>
+            <a href="todo" class="navbar-item">Игры</a>
+            <a href="todo" class="navbar-item">Сеттинги</a>
+            <a href="todo" class="navbar-item">👁️</a>
+        </div>
+    </nav>
+</main>
+
+<style>
+</style>
